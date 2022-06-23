7c5d4f89395232b3065adb0a06fdcad42027194a
Michael Smith <mikeytsmith@gmail.com>
2022-06-23 14:21:17 +0100

    MS: test to see google functions and and automation website shop worked

--- testAutomation.spec.js
+++ testAutomation.spec.js
@@ -4,12 +4,13 @@ const {Builder, By, Key} = require("selenium-webdriver");
 // Key - keyboard/mouse functionality
 const assert = require("assert"); // for our actual test - assertions
 
+// using mocha to run describe test and adda function without a timeout
 describe("check to click the tshirt button and click on  t-shirt", function() {
-    this.timeout(30000);
+    this.timeout(300000);
 
     let driver; //declaring a driver variable
 
-    //setting up our browser to be ready for testing - before each test
+    //setting up our browser to be ready for testing - before each test. creates driver window for each test
     beforeEach(async () => {
         driver = await new Builder().forBrowser("chrome").build();
         driver.manage().setTimeouts({implicit:5000});
@@ -20,39 +21,71 @@ describe("check to click the tshirt button and click on  t-shirt", function() {
         await driver.quit();
     });
 
-    // tests
+    // tests is the individual test with description and async function declaration
     it("Go to automationpractice and click TShirt ", async () => {
+        // arrange
+        //tells driver to go to automation website
         driver.get("http://automationpractice.com/index.php"); // navigate to the URL declared
 
         let tshirt;
-        let tshirtToText;
+        let tshirtText;
 
-        //click t shirt button
-        driver.findElement(By.xpath("/html/body/div/div[1]/header/div[3]/div/div/div[6]/ul/li[3]/a")).click(); //accept the agreeement
-        await driver.findElement(By.xpath("/html/body/div/div[1]/header/div[3]/div/div/div[6]/ul/li[3]/a")).click(); //find the button and click it
+        // Act
+    // Finding the tshirt link and clicking it
+    tshirt = await driver.findElement(By.xpath
+        ("/html/body/div/div[1]/header/div[3]/div/div/div[6]/ul/li[3]/a"))
+        .click();
+  
+  //comparing xpath text with expected text
+      tshirtText = await
+        (await driver.findElement
+          (By.xpath("/html/body/div/div[2]/div/div[3]/div[2]/h1/span[1]"))).getText()
+          .then(function (value) {
+            return value
+          });
+  // assert.equal(tshirtText, "T-SHIRTS") 
        
         // click the first t shirt that loads
-        await driver.findElement(By.className("button lnk_view btn btn-default")).click();
-       
-        // get the products name (H1 tag)
-        tshirt = await driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div/div[3]/h1"));
+         //click on 1st t-shirt on screen
+    clickTshirt = await driver.findElement
+    (By.xpath("/html/body/div/div[2]/div/div[3]/div[2]/ul/li/div/div[1]/div/a[1]/img"))
+    .click();
 
+    //compare xpath text with what's expected -  get the products name (H1 tag)
+  let chosenShirt = await
+    (await driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div/div[3]/h1"))).getText()
+      .then(function (value) {
+        return value
+      });
+
+//   assert.equal(chosenShirt, "Faded Short Sleeve T-shirts");
+      
         //stores this h1 tag as text
-        tshirtToText = await product.getText();
+        // tshirtToText = await chosenShirt.getText();
 
         // clicks add to cart
-        await driver.findElement(By.name="Submit").click();
-
+        addToCart = await
+            (await driver.findElement(By.name("Submit"))).click();
+            // xpath("/html/body/div/div[2]/div/div[3]/div/div/div/div[4]/form/div/div[3]/div/p/button
         //on popup window click proceed to checkout
-        await driver.findElement(By.className("btn btn-default button button-medium")).click();
+        popUp = await
+        (await driver.findElement
+          (By.className("btn btn-default button button-medium")))
+            .click();
+        // xpath ("/html/body/div/div[1]/header/div[3]/div/div/div[4]/div[1]/div[2]/div[4]/a/span
 
-        let basketItem;
-        let basketItemText;
-        
-        //stores product name in checkout to variable
-        basketItem = await driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div[2]/table/tbody/tr/td[2]/p/a"));
-        basketItemText = await basketItem.getText();
-        
-        assert.equal(tshirtToText, basketItemText); // to check if the value we got is equal to what we expected!
-    });
+        //compare xpath text vs what's expected
+    addedToCart = await
+    (await driver.findElement
+      (By.xpath
+        ("/html/body/div/div[2]/div/div[3]/div/div[2]/table/tbody/tr/td[2]/p/a")))
+        .getText()
+        .then (function (value) {
+          return value
+        });
+       
+
+    assert.equal(addedToCart, chosenShirt);
+  });
 });
+      
